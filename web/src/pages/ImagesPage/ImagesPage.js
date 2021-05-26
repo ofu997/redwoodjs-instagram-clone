d7648c08acff80f080582a3d1ef17845935bff1f
oliver fu <ofu997@gmail.com>
2021-05-25 20:07:09 -0700

    add function to useEffect, pass user.id as currentUserId instead of manual value

--- web/src/pages/ImagesPage/ImagesPage.js
+++ web/src/pages/ImagesPage/ImagesPage.js
@@ -1,18 +1,28 @@
 import ImagesLayout from 'src/layouts/ImagesLayout'
 import ImagesCell from 'src/components/ImagesCell'
 import Header from '../../components/Header/Header'
+import { useState, useEffect } from 'react';
+import { getLoggedInUser } from 'src/functions/GetLoggedInUser'
 
 const ImagesPage = () => {
+  const [user, setUser] = useState({ });
+
+  useEffect(() => {
+    const localStorageUser = getLoggedInUser();
+    setUser(localStorageUser);
+  }, [])
+
   return (
     <>
       <Header />
       <ImagesLayout>
-        <ImagesCell currentUserId={currentUserId} />
+        {/* if not logged in, should show a carousel or images with fewer details */}
+        {
+          <ImagesCell currentUserId={user.id} />
+        }
       </ImagesLayout>
     </>
   )
 }
-
-const currentUserId = 1;
 
 export default ImagesPage
