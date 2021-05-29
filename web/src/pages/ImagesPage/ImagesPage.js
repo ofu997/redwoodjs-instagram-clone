c837d6a2372bed0a20ea8160a76eddfc1ba4de74
oliver fu <ofu997@gmail.com>
2021-05-28 20:43:37 -0700

    delete useEffect for retrieving logged-in user

--- web/src/pages/ImagesPage/ImagesPage.js
+++ web/src/pages/ImagesPage/ImagesPage.js
@@ -1,16 +1,10 @@
 import ImagesLayout from 'src/layouts/ImagesLayout'
 import ImagesCell from 'src/components/ImagesCell'
 import Header from '../../components/Header/Header'
-import { useState, useEffect } from 'react';
 import { getLoggedInUser } from 'src/functions/GetLoggedInUser'
 
 const ImagesPage = () => {
-  const [user, setUser] = useState({ });
-
-  useEffect(() => {
-    const localStorageUser = getLoggedInUser();
-    setUser(localStorageUser);
-  }, [])
+  const user = getLoggedInUser();
 
   return (
     <>
--- web/src/pages/UserPage/UserPage.js
+++ web/src/pages/UserPage/UserPage.js
@@ -1,22 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
 import UserCell from '../../components/UserCell'
 import Header from 'src/components/Header/Header'
-import { useState, useEffect } from 'react';
-import { getLoggedInUser } from 'src/functions/GetLoggedInUser'
 
-const UserPage = () => {
-  const [user, setUser] = useState({ });
-
-  useEffect(() => {
-    const localStorageUser = getLoggedInUser();
-      setUser(localStorageUser);
-    }, [])
-
+const UserPage = props => {
   return (
     <>
       <Header />
       <Link to={routes.images()}><p>Go back</p></Link>
-      <UserCell id={user.id} />
+      <UserCell id={props.id} />
     </>
   )
 }
