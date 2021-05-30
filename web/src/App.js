b4bd2828cd5bdee154349a924e47671015c90584
oliver fu <ofu997@gmail.com>
2021-05-30 13:43:55 -0700

    upgrade to redwood 0.26.0

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,18 @@
-import ReactDOM from 'react-dom'
 import { FatalErrorBoundary } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
-import FatalErrorPage from 'src/pages/FatalErrorPage'
 
+import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './custom-styles.css'
 import './index.css'
 
-ReactDOM.render(
+const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodApolloProvider>
       <Routes />
     </RedwoodApolloProvider>
-  </FatalErrorBoundary>,
-  document.getElementById('redwood-app')
+  </FatalErrorBoundary>
 )
+
+export default App
