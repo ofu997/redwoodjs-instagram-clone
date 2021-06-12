37fa257c5dcde0736502c34c59e6dd2da967b6e7
oliver fu <ofu997@gmail.com>
2021-06-12 11:57:21 -0700

    web: redefine routes for HomePage and ImagesPage, add UserPage and EditUserInfoPage

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -14,12 +14,13 @@ const Routes = () => {
 
   return (
     <Router>
-      <Route path="/user/{id:Int}" page={UserPage} name="userPage" />
-      <Route path="/" page={HomePage} name='homePage' />
+      <Route path="/" page={ImagesPage} name="images" />
+      <Route path="/u/{handle:String}/edit" page={EditUserInfoPage} name="editUserInfo" />
+      <Route path="/u/{handle:String}" page={UserPage} name="userPage" />
+      <Route path="/log" page={HomePage} name='homePage' />
       <Route path="/images/new" page={NewImagePage} name="newImage" />
       <Route path="/images/{id:Int}/edit" page={EditImagePage} name="editImage" />
       <Route path="/images/{id:Int}" page={ImagePage} name="image" />
-      <Route path="/images" page={ImagesPage} name="images" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
