2119e12d33b08e0d91f343705849e287abfeb218
oliver fu <ofu997@gmail.com>
2021-06-14 21:34:37 -0700

    expand EditUser query and mutation

--- web/src/components/EditUserInfoCell/EditUserInfoCell.js
+++ web/src/components/EditUserInfoCell/EditUserInfoCell.js
@@ -1,11 +1,24 @@
+import { navigate, routes } from '@redwoodjs/router'
+import { useMutation } from '@redwoodjs/web'
+import toast from 'react-hot-toast'
+import EditUserInfoForm from 'src/components/UserForms/EditUserInfoForm'
+
 export const QUERY = gql`
-  query doesnotmatter($handle: String!) {
-    editUserInfo: findUserByHandle(handle: $handle) {
+  query findUserByHandle($handle: String!) {
+    userToEdit: findUserByHandle(handle: $handle) {
       id
       name
       handle
       bio
       profilePicUrl
+    }
+  }
+`
+
+const EDIT_USER_INFO_MUTATION = gql`
+  mutation EditUserInfoMutation($id: Int!, $input: CreateOrUpdateUserInfo!) {
+    createOrUpdateUserInfo(id: $id, input: $input) {
+      id
     }
   }
 `
@@ -18,8 +31,30 @@ export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-export const Success = ({ editUserInfo }) => {
+export const Success = ({ userToEdit }) => {
+  const [editUserInfo, {loading, error}] = useMutation(EDIT_USER_INFO_MUTATION, {
+    onCompleted: () => {
+      navigate( routes.userPage({handle: userToEdit.handle}) )
+      toast.success('Info updated', { classes: 'rw-flash-success' })
+    }
+  })
+
+  const onSave = (input, id) => {
+    editUserInfo({ variables: { id, input }})
+  }
+
   return(
-    <h1>success for {editUserInfo.handle} </h1>
+    <>
+      <div className="rw-segment">
+        <div className="rw-segment-main">
+          <EditUserInfoForm
+            user={userToEdit}
+            onSave={onSave}
+            error={error}
+            loading={loading}
+          />
+        </div>
+      </div>
+    </>
   )
 }
