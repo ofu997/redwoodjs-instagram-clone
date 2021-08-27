c436e5c7cd99814e5548e0d9e6885aff689e6b55
oliver fu <ofu997@gmail.com>
2021-08-27 00:31:56 -0700

    add profile link to comments, delete redundant logic

--- web/src/components/Comment/Comment.js
+++ web/src/components/Comment/Comment.js
@@ -3,6 +3,8 @@ import { QUERY } from 'src/components/ImagesCell'
 import { toast } from '@redwoodjs/web/toast'
 import jwt from 'jsonwebtoken'
 import { Trash } from 'react-bootstrap-icons'
+import { Link, routes } from '@redwoodjs/router'
+
 const DELETE_COMMENT_MUTATION = gql`
   mutation DeleteCommentMutation($id: Int!) {
     deleteComment(id: $id) {
@@ -22,51 +24,50 @@ const Comment = props => {
   const missingData = !props.user ? true : false;
 
   const handleDelete = id => {
-    // user made the comment: comment posterId = userId (check LS and uQ match)
-    // image belongs to user
-    // isAdmin
-    (
-      ( props.comment.user.id === props.LSuser.id && props.LSuser.localStoragePassword === props.user.localStoragePassword )
-      ||
-      props.user.images.some(image => image.id === props.comment.imageId)
-      ||
-      ( props.user.isAdmin && props.LSuser.localStoragePassword === props.user.localStoragePassword )
-    )
-      ? jwt.verify(props.user.jwt, `${process.env.MY_SECRET}`, function(err) {
-          if (err) {
-            toast.error('Please log in again')
-          }
-          else {
-            if (confirm('Delete comment?')) {
-              deleteComment({variables: {id} })
-            }
-          }
-        })
-      :
-      toast.error("Womp womp...invalid credentials")
+    jwt.verify(props.user.jwt, `${process.env.MY_SECRET}`, function(err) {
+      if (err) {
+        toast.error('Please log in again')
+      }
+      else {
+        if (confirm('Delete comment?')) {
+          deleteComment({variables: {id} })
+        }
+      }
+    })
   }
 
   return (
     <div style={{ display: 'flex', alignItems: 'center', fontSize: '0.9rem' }}>
-      <p className='rc-font-size'>{props.comment.user.handle}: {props.comment.body}</p>
-      {(
-        ( props.comment.user?.id === props.LSuser?.id && props.LSuser.localStoragePassword === props.user?.localStoragePassword )
-        ||
-        props.user?.images.some(image => image.id === props.comment.imageId)
-        ||
-        ( props.user?.isAdmin && props.LSuser.localStoragePassword === props.user?.localStoragePassword )
-      ) && (
-        <Trash
-          onClick={() => missingData
-            ? (
-            toast.error("Must be logged in to delete comment")
-            )
-            : handleDelete(props.comment.id)
-          }
-          size={15}
-          id='comment-delete'
-        />
-      )}
+      <p className='rc-font-size'>
+        <Link
+          to={routes.userPage({ handle : props.comment.user.handle })}
+          className='link-that-does-not-look-like-a-link'
+        ><span style={{ fontWeight: 500, marginRight: 10 }}>{props.comment.user.handle}</span>
+        </Link>
+        {props.comment.body}</p>
+      {
+        // user made the comment: comment posterId = userId (check LS and uQ match)
+        // image belongs to user
+        // isAdmin
+        (
+          ( props.comment.user?.id === props.LSuser?.id && props.LSuser.localStoragePassword === props.user?.localStoragePassword )
+          ||
+          props.user?.images.some(image => image.id === props.comment.imageId)
+          ||
+          ( props.user?.isAdmin && props.LSuser.localStoragePassword === props.user?.localStoragePassword )
+        ) && (
+          <Trash
+            onClick={() => missingData
+              ? (
+              toast.error("Must be logged in to delete comment")
+              )
+              : handleDelete(props.comment.id)
+            }
+            size={15}
+            id='comment-delete'
+          />
+        )
+      }
     </div>
   )
 }
