6541e76b68a77ac3fddde85c53be5e1fc41a5dad
oliver fu <ofu997@gmail.com>
2022-07-20 21:29:08 -0700

    Add meta tags (2nd attempt)

--- web/src/components/Image/Image.js
+++ web/src/components/Image/Image.js
@@ -1,5 +1,6 @@
 import Images from 'src/components/Images/Images'
 import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
 
 const Image = ({ image }) => {
 
@@ -7,6 +8,12 @@ const Image = ({ image }) => {
 
   return (
     <>
+      <MetaTags
+        title={`${image.title.substring(0, 75)} | ${image.user.name}`}
+        ogType='website'
+        ogUrl={`https://memofolio.netlify.app/memo/${image.id}`}
+        ogContentUrl={image.url}
+      />
       <Link
         to={routes.userPage({ handle : image.user.handle })}
         className='link-that-does-not-look-like-a-link'
--- web/src/components/ImageCell/ImageCell.js
+++ web/src/components/ImageCell/ImageCell.js
@@ -14,6 +14,7 @@ export const QUERY = gql`
       user {
         handle
         profilePicUrl
+        name
       }
 
       userId
