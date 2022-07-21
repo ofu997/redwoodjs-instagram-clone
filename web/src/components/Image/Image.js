bef25c2f22d0b2caf8ef1a97fa15a2e59191292e
oliver fu <ofu997@gmail.com>
2022-07-20 20:15:18 -0700

    Add meta tags for link preview

--- web/src/components/Image/Image.js
+++ web/src/components/Image/Image.js
@@ -1,5 +1,7 @@
 import Images from 'src/components/Images/Images'
 import { Link, routes } from '@redwoodjs/router'
+import { Head } from '@redwoodjs/web'
+import { MetaTags } from '@redwoodjs/web'
 
 const Image = ({ image }) => {
 
@@ -7,6 +9,12 @@ const Image = ({ image }) => {
 
   return (
     <>
+      <MetaTags
+        title={`{image.title.substring(0, 75)} | {image.user.name}`}
+        ogType='website'
+        ogUrl={`https://memofolio.netlify.app/memo/{image.id}`}
+        ogContentUrl={image.url}
+      />
       <Link
         to={routes.userPage({ handle : image.user.handle })}
         className='link-that-does-not-look-like-a-link'
