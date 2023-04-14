497b02c6b2148b8cbc7d182e82a65e765f8ece94
oliver fu <ofu997@gmail.com>
2023-04-14 06:31:03 -0700

    upgrade to 0.47

--- web/src/components/CommentsCell/CommentsCell.mock.js
+++ web/src/components/CommentsCell/CommentsCell.mock.js
@@ -1,6 +1,8 @@
 // Define your own mock data here:
-export const standard = (/* vars, { ctx, req } */) => ({
-  comments: {
-    id: 42,
-  },
-})
+export const standard = () => {
+  return {
+    comments: {
+      id: 42,
+    },
+  };
+};
